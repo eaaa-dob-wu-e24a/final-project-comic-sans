c4783b1cd40078a8abcecfc94b692420ac887fc1
André Zacharov <eaa24anza@students.eaaa.dk>
2024-11-21 09:32:59 +0100

    branch rename

--- backend/index.php
+++ backend/index.php
@@ -3,4 +3,4 @@ require_once __DIR__ . "/loadenv.php"; // load the env function from file
 
 $dotenv = loadenv('.env'); // load the .env file from path
 
-echo $dotenv['DB_USER'];  //example of how to access variables from .env file
+echo $dotenv['DB_USER'];  //example of how to access variables from the .env file

b99a24b8a6e09198f2fc36242fa09fc13350df0c
siirielli <eaasesli@students.eaaa.dk>
2024-11-26 19:52:42 +0100

    Update styles and layout for login and home pages; add background SVG

--- frontend/eventually/app/page.jsx
+++ frontend/eventually/app/page.jsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
+
 export default function Home() {
   return (
-   <main>
-      <h1 className="font-[family-name:var(--font-dancing-script)] text-6xl">Eventually</h1>
-      <p>Let’s make it happen..eventually.</p>
-      <Link href="/login/" className="border p-2 rounded-full">placeholder login button</Link>
-   </main>
+    <main className="relative top-0 min-h-screen pt-36 bg-[url('/background1.svg')] bg-top bg-no-repeat flex flex-col items-center justify-start text-white">
+      <h1 className="font-[family-name:var(--font-dancing-script)] text-7xl mb-4">
+        Eventually
+      </h1>
+      <p className="text-lg mb-6">Let’s make it happen..eventually.</p>
+      <Link
+        href="/login/"
+        className="border p-2 rounded-full bg-white text-black"
+      >
+        Placeholder Login Button
+      </Link>
+    </main>
   );
 }

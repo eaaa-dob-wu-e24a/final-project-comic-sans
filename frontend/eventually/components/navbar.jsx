746e94bbca9cd6a475d90051c262073ebaa4f383
siirielli <eaasesli@students.eaaa.dk>
2024-11-26 17:28:40 +0100

    Add Navbar component with links and create event button

---
+++ frontend/eventually/components/navbar.jsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import Link from "next/link"; // Corrected import
+import Button from "../components/button"; // Ensure correct casing and path
+
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between items-center px-20 py-5 text-white">
+            <div className="flex items-center gap-6">
+                <h1 className="font-[family-name:var(--font-dancing-script)] text-3xl">
+                    Eventually
+                </h1>
+            </div>
+            <div className="flex gap-10">
+                <Link href="/login" className="flex items-center w-20 whitespace-nowrap">
+                    LOGIN
+                </Link>
+                <Link href="/signup" className="flex items-center w-20 whitespace-nowrap">
+                    SIGN UP
+                </Link>
+                <Button label="CREATE EVENT" className="m-10" />
+            </div>
+        </nav>
+    );
+}

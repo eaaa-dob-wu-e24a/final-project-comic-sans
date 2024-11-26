d91442e59e11abb6d35c22f7104b866679880718
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2024-11-26 13:52:43 +0100

    Update user events list with data from each event

--- frontend/eventually/components/user-event-list.jsx
+++ frontend/eventually/components/user-event-list.jsx
@@ -9,44 +9,76 @@ export default function UserEventList(id) {
 
   useEffect(() => {
     fetch(url, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-          }
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then(response => response.json())
-    .then(data => {
-        if(data) {
-            console.log(data.events.length + " events found.");
-            setEvents(data.events);
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          console.log(data.events[0].EventDates[0].DateTimeStart);
+          setEvents(data.events);
         } else {
-            console.log("no data yet.")
+          console.log("no data yet.");
         }
-    })
-    .catch(error => console.log(error))
-    .finally(() => setLoading(false));
-
-  }, [])
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <section className="mx-auto flex flex-col">
-        <div>
-      <h2>Your Events</h2>
-        </div>
+      <div>
+        <h2>Your Events</h2>
+      </div>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <ul className="flex flex-row  gap-2">
-            {events.map(event => (
-                <li className="bg-white rounded-2xl text-black p-4 shadow-md flex basis-0 grow shrink-0 place-content-center" key={event.PK_ID}>
-                    <p>{event.Title}</p>
-                    <p>{event.FinalDate}</p>
-                </li>
-            ))}
+        <ul className="flex flex-row  gap-4">
+          {events.map((event) => (
+            <li
+              className="bg-white rounded-2xl text-black p-4 shadow-md flex flex-col basis-0 grow shrink-0 place-content-center"
+              key={event.PK_ID}
+            >
+              <div className="flex flex-col place-items-center">
+                <p className="text-sm font-bold opacity-60">
+                  {event.EventDates && event.EventDates.length > 0
+                    ? new Date(
+                        event.EventDates[0].DateTimeStart
+                      ).toLocaleString("default", {
+                        weekday: "long"
+                      })
+                    : "N/A"}
+                </p>
+                <p className="text-gradientend my-[-0.25rem] text-4xl font-bold">
+                {event.EventDates && event.EventDates.length > 0
+                    ? new Date(
+                        event.EventDates[0].DateTimeStart
+                      ).toLocaleString("default", {
+                        day: "2-digit"
+                      })
+                    : "N/A"}
+                </p>
+                <p className="font-bold  opacity-60 text-lg">
+                {event.EventDates && event.EventDates.length > 0
+                    ? new Date(
+                        event.EventDates[0].DateTimeStart
+                      ).toLocaleString("default", {
+                        month: "short"
+                      })
+                    : "N/A"}
+                </p>
+              </div>
+
+              <p className="mx-auto font-bold text-2xl capitalize">
+                {event.Title}
+              </p>
+            </li>
+          ))}
         </ul>
-      )
-      }
+      )}
     </section>
   );
 }

1c2dc8a93b7ed00bdcc8bfa8b5078f68e5eea2f7
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2024-11-26 14:39:44 +0100

    add login button placeholder

--- frontend/eventually/app/page.jsx
+++ frontend/eventually/app/page.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 export default function Home() {
   return (
    <main>
       <h1 className="font-[family-name:var(--font-dancing-script)] text-6xl">Eventually</h1>
       <p>Let’s make it happen..eventually.</p>
+      <Link href="/login/" className="border p-2 rounded-full">placeholder login button</Link>
    </main>
   );
 }

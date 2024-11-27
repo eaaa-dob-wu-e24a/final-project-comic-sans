d25e12b0d01a6ad0226bf9ec80c8005d834d04dc
Simone <simi@youseepost.dk>
2024-11-27 13:04:14 +0100

    Refactor global CSS variables and reorganize curve-gradient styles for improved readability

--- frontend/eventually/app/createEvent/page.jsx
+++ frontend/eventually/app/createEvent/page.jsx
@@ -67,123 +67,134 @@ const CreateEvent = () => {
     };
 
     return (
-        <div className="flex flex-col p-4 space-y-4">
-            <h1 className="font-extrabold text-2xl mb-4">Create Event</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="title" className="block text-lg font-medium">Event Name:</label>
-                    <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        className="mt-1 px-4 py-2 text-black bg-gray-300 rounded"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="description" className="block text-lg font-medium">Description:</label>
-                    <input
-                        type="text"
-                        id="description"
-                        name="description"
-                        className="mt-1 px-4 py-2 text-black bg-gray-300 rounded"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="location" className="block text-lg font-medium">Location:</label>
-                    <input
-                        type="text"
-                        id="location"
-                        name="location"
-                        className="mt-1 px-4 py-2 text-black bg-gray-300 rounded"
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="joinCode" className="block text-lg font-medium">Join Code:</label>
-                    <input
-                        type="text"
-                        id="joinCode"
-                        name="joinCode"
-                        className="mt-1 px-4 py-2 text-black bg-gray-300 rounded"
-                        value={joinCode}
-                        onChange={(e) => setJoinCode(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="userName" className="block text-lg font-medium">User Name:</label>
-                    <input
-                        type="text"
-                        id="userName"
-                        name="userName"
-                        className="mt-1 px-4 py-2 text-black bg-gray-300 rounded"
-                        value={userName}
-                        onChange={(e) => setUserName(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="finalDate" className="block text-lg font-medium">Final Date:</label>
-                    <input
-                        type="date"
-                        id="finalDate"
-                        name="finalDate"
-                        className="mt-1 px-4 py-2 text-black bg-gray-300 rounded"
-                        value={finalDate}
-                        onChange={(e) => setFinalDate(e.target.value)}
-                    />
-                </div>
+        <div className="flex flex-col px-4 pt-16 pb-8 space-y-4 bg-gradient-to-r from-gradientstart to-gradientend pb-0">
 
-                {/* Proposed Dates Section */}
-                <div>
-                    <h3 className="mt-4 text-lg font-medium">Proposed Dates:</h3>
-                    {proposedDates.map((date, index) => (
-                        <div key={index} className="flex space-x-2 mt-2">
-                            <input
-                                type="datetime-local"
-                                value={date.start}
-                                onChange={(e) => handleProposedDateChange(index, 'start', e.target.value)}
-                                className="flex-1 px-4 py-2 text-black bg-gray-300 rounded"
-                                placeholder="Start Date"
-                            />
-                            <input
-                                type="datetime-local"
-                                value={date.end}
-                                onChange={(e) => handleProposedDateChange(index, 'end', e.target.value)}
-                                className="flex-1 px-4 py-2 text-black bg-gray-300 rounded"
-                                placeholder="End Date"
-                            />
-                            <button
-                                type="button"
-                                onClick={() => removeProposedDate(index)}
-                                className="px-2 py-1 bg-red-500 text-white rounded"
-                            >
-                                Remove
-                            </button>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={addProposedDate}
-                        className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
-                    >
-                        Add Proposed Date
+            <section className="max-w-lg mx-auto flex flex-col text-white place-content-center text-center  ">
+                <h1 className="font-extrabold text-2xl mb-4">Create Event</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="title" className="block text-lg font-medium">Event Name:</label>
+                        <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            placeholder='Event Name'
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="description" className="block text-lg font-medium">Description:</label>
+                        <input
+                            type="text"
+                            id="description"
+                            name="description"
+                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder='Description'
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="location" className="block text-lg font-medium">Location:</label>
+                        <input
+                            type="text"
+                            id="location"
+                            name="location"
+                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                            placeholder='Location'
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="joinCode" className="block text-lg font-medium">Join Code:</label>
+                        <input
+                            type="text"
+                            id="joinCode"
+                            name="joinCode"
+                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            value={joinCode}
+                            onChange={(e) => setJoinCode(e.target.value)}
+                            placeholder='Join Code'
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="userName" className="block text-lg font-medium">User Name:</label>
+                        <input
+                            type="text"
+                            id="userName"
+                            name="userName"
+                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                            placeholder='User Name'
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="finalDate" className="block text-lg font-medium">Final Date:</label>
+                        <input
+                            type="date"
+                            id="finalDate"
+                            name="finalDate"
+                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            value={finalDate}
+                            onChange={(e) => setFinalDate(e.target.value)}
+                            placeholder='Final Date'
+                        />
+                    </div>
+
+                    {/* Proposed Dates Section */}
+                    <div>
+                        <h3 className="mt-4 text-lg font-medium">Proposed Dates:</h3>
+                        {proposedDates.map((date, index) => (
+                            <div key={index} className="flex space-x-2 mt-2">
+                                <input
+                                    type="datetime-local"
+                                    value={date.start}
+                                    onChange={(e) => handleProposedDateChange(index, 'start', e.target.value)}
+                                    className="rounded-full flex-1 px-4 py-2 text-black bg-gray-300 rounded"
+                                    placeholder="Start Date"
+                                />
+                                <input
+                                    type="datetime-local"
+                                    value={date.end}
+                                    onChange={(e) => handleProposedDateChange(index, 'end', e.target.value)}
+                                    className="rounded-full flex-1 px-4 py-2 text-black bg-gray-300 rounded"
+                                    placeholder="End Date"
+                                />
+                                <button
+                                    type="button"
+                                    onClick={() => removeProposedDate(index)}
+                                    className="rounded-full px-2 py-1 bg-red-500 text-background rounded"
+                                >
+                                    Remove
+                                </button>
+                            </div>
+                        ))}
+                        <button
+                            type="button"
+                            onClick={addProposedDate}
+                            className="rounded-full mt-2 px-4 py-2 bg-blue-500 text-white"
+                        >
+                            Add Proposed Date
+                        </button>
+                    </div>
+
+                    <button type="submit" className="rounded-full mt-4 px-4 py-2 text-white bg-gray-800">
+                        Create Event
                     </button>
-                </div>
+                </form>
 
-                <button type="submit" className="mt-4 px-4 py-2 text-white bg-gray-800 rounded">
-                    Create Event
-                </button>
-            </form>
+                {responseMessage && (
+                    <div className={`mt-4 ${responseMessage.success ? 'text-green-500' : 'text-red-500'}`}>
+                        {responseMessage.message}
+                    </div>
+                )}
 
-            {responseMessage && (
-                <div className={`mt-4 ${responseMessage.success ? 'text-green-500' : 'text-red-500'}`}>
-                    {responseMessage.message}
-                </div>
-            )}
+            </section>
+
         </div>
     );
 };

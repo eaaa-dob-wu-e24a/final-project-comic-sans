e9783f6963b152d883815ae95fdb24c3f23efde2
Simone <simi@youseepost.dk>
2024-11-27 14:06:19 +0100

    Refactor CreateEvent component layout and styles; enhance input fields and add description textarea

--- frontend/eventually/app/createEvent/page.jsx
+++ frontend/eventually/app/createEvent/page.jsx
@@ -67,9 +67,9 @@ const CreateEvent = () => {
     };
 
     return (
-        <div className="flex flex-col px-4 pt-16 pb-8 space-y-4 bg-gradient-to-r from-gradientstart to-gradientend pb-0">
+        <div className="flex flex-col px-4 pt-16 pb-8 bg-gradient-to-r from-gradientstart to-gradientend pb-0">
 
-            <section className="max-w-lg mx-auto flex flex-col text-white place-content-center text-center  ">
+            <section className="space-y-4 p-12 rounded-xl drop-shadow mx-auto flex flex-col text-black place-content-center bg-pagebackground">
                 <h1 className="font-extrabold text-2xl mb-4">Create Event</h1>
                 <form onSubmit={handleSubmit}>
                     <div>
@@ -78,31 +78,20 @@ const CreateEvent = () => {
                             type="text"
                             id="title"
                             name="title"
-                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            className="max-w-lg rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
                             placeholder='Event Name'
                         />
                     </div>
-                    <div>
-                        <label htmlFor="description" className="block text-lg font-medium">Description:</label>
-                        <input
-                            type="text"
-                            id="description"
-                            name="description"
-                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                            placeholder='Description'
-                        />
-                    </div>
+
                     <div>
                         <label htmlFor="location" className="block text-lg font-medium">Location:</label>
                         <input
                             type="text"
                             id="location"
                             name="location"
-                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            className="max-w-lg rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
                             value={location}
                             onChange={(e) => setLocation(e.target.value)}
                             placeholder='Location'
@@ -114,7 +103,7 @@ const CreateEvent = () => {
                             type="text"
                             id="joinCode"
                             name="joinCode"
-                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            className="max-w-lg rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
                             value={joinCode}
                             onChange={(e) => setJoinCode(e.target.value)}
                             placeholder='Join Code'
@@ -126,7 +115,7 @@ const CreateEvent = () => {
                             type="text"
                             id="userName"
                             name="userName"
-                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            className="max-w-lg rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
                             value={userName}
                             onChange={(e) => setUserName(e.target.value)}
                             placeholder='User Name'
@@ -138,7 +127,7 @@ const CreateEvent = () => {
                             type="date"
                             id="finalDate"
                             name="finalDate"
-                            className="rounded-full mt-1 px-4 py-2 text-black bg-gray-300 rounded"
+                            className="max-w-lg rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
                             value={finalDate}
                             onChange={(e) => setFinalDate(e.target.value)}
                             placeholder='Final Date'
@@ -154,14 +143,14 @@ const CreateEvent = () => {
                                     type="datetime-local"
                                     value={date.start}
                                     onChange={(e) => handleProposedDateChange(index, 'start', e.target.value)}
-                                    className="rounded-full flex-1 px-4 py-2 text-black bg-gray-300 rounded"
+                                    className="max-w-lg rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
                                     placeholder="Start Date"
                                 />
                                 <input
                                     type="datetime-local"
                                     value={date.end}
                                     onChange={(e) => handleProposedDateChange(index, 'end', e.target.value)}
-                                    className="rounded-full flex-1 px-4 py-2 text-black bg-gray-300 rounded"
+                                    className="max-w-lg rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
                                     placeholder="End Date"
                                 />
                                 <button
@@ -180,6 +169,19 @@ const CreateEvent = () => {
                         >
                             Add Proposed Date
                         </button>
+                    </div>
+
+                    <div className='flex flex-col min-h-[4rem] w-full'>
+                        <label htmlFor="description" className="block  text-lg text-black font-medium">Description:</label>
+                        <textarea
+                            type="text"
+                            id="description"
+                            name="description"
+                            className="rounded-xl w-full mt-1 px-4 py-2 text-black rounded border-2 border-grey-300"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder='Description'
+                        />
                     </div>
 
                     <button type="submit" className="rounded-full mt-4 px-4 py-2 text-white bg-gray-800">

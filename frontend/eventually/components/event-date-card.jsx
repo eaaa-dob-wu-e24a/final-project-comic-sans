0baf079f297b77ac1b5f37f7a7bded7b67715c39
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2024-11-27 15:59:18 +0100

    add gradient border to event date cards

--- frontend/eventually/components/event-date-card.jsx
+++ frontend/eventually/components/event-date-card.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function DateCard({ time, title }) {
   const cardTime = new Date(time);
+
+  const beforeClasses = "before:bg-background before:content-[''] before:inset-[1px] before:absolute before:rounded-2xl hover:before:opacity-0 hover:before:transition-all before:duration-500 hover:before:duration-500" // remember to set z-index on content, otherwise it goes behind the pseudo-element
+
   console.log(time);
   return (
-    <li className="bg-background text-foreground rounded-2xl text-black p-4 shadow-lg flex flex-col basis-0 grow shrink-0 place-content-center max-w-48 border border-primary">
-      <div className="flex flex-col place-items-center">
+    <li className={`bg-gradient-to-r from-gradientstart to-gradientend text-foreground rounded-2xl p-4 shadow-lg flex flex-col basis-0 grow shrink-0 place-content-center max-w-48 relative hover:cursor-pointer ${beforeClasses}`}>
+      <div className="flex flex-col place-items-center z-10">
         <p className="text-sm font-bold opacity-60">
           {time
             ? new Date(time).toLocaleString("default", {
@@ -31,7 +34,7 @@ export default function DateCard({ time, title }) {
         </p>
       </div>
 
-      <p className="mx-auto font-bold text-2xl capitalize">{title}</p>
+      <p className="mx-auto font-bold text-2xl capitalize z-10">{title}</p>
     </li>
   );
 }

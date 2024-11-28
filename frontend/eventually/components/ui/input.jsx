21c8be42a4d4f1220661038bdb33979c3fae247a
siirielli <eaasesli@students.eaaa.dk>
2024-11-28 14:16:43 +0100

    Added onChange prop and styling for input field component

--- frontend/eventually/components/ui/input.jsx
+++ frontend/eventually/components/ui/input.jsx
@@ -1,12 +1,13 @@
-export default function Input({ type, placeholder, id, maxLength, className }) {
+export default function Input({ type, placeholder, id, maxLength, className, onChange}) {
   return (
     <input
       type={type}
       placeholder={placeholder}
       maxLength={maxLength}
-      className={`rounded-full text-background py-2 px-4 ${className}`}
+      className={`rounded-full text-background py-2 px-4 w-full focus:border-secondary focus:ring-1 focus:ring-secondary focus:bg-white/80 focus:outline-none ${className}`}
       name={id}
       id={id}
+      onChange={onChange}
     />
   );
 }

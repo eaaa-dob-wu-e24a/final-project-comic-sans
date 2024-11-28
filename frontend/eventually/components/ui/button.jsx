22770d36c2ab4e9b5039d4cbf0ff2e105881119e
siirielli <eaasesli@students.eaaa.dk>
2024-11-28 14:58:45 +0100

    Replace button component in navbar

--- frontend/eventually/components/ui/button.jsx
+++ frontend/eventually/components/ui/button.jsx
@@ -3,7 +3,7 @@ export default function Button({ children, className, type, onClick }) {
     <button
       type={type}
       onClick={onClick}
-      className={`${className} bg-primary text-white text-xl font-bold py-2 px-8  uppercase rounded-full shadow-md hover:cursor-pointer`}
+      className={`${className} bg-primary text-white text-m font-bold py-2 px-8 uppercase rounded-full shadow-md hover:cursor-pointer`}
     >
       {children}
     </button>

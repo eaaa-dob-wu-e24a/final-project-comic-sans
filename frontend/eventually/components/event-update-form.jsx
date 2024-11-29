45f9b1c5cdbc918677a36c9205c243b112be7f69
André Zacharov <eaa24anza@students.eaaa.dk>
2024-11-29 12:22:01 +0100

    fix update event

--- frontend/eventually/components/event-update-form.jsx
+++ frontend/eventually/components/event-update-form.jsx
@@ -1,15 +1,16 @@
 "use client";
 import React, { useState } from "react";
 
-export default function EventUpdateForm() {
+export default function EventUpdateForm({ id }) {
   const [data, setData] = useState(null); // State to store response data or errors
+  const eventID = id;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const url = process.env.NEXT_PUBLIC_API_URL + "/api/event/update/";
     const formValues = {
-      ID: document.getElementById("eventid").value, //change this to be fetched automatically on event page
+      ID: eventID,
       Title: document.getElementById("newtitle").value,
       Description: document.getElementById("newdesc").value,
       Location: document.getElementById("newloc").value,
@@ -40,9 +41,6 @@ export default function EventUpdateForm() {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col mx-auto max-w-96">
-      <label htmlFor="eventid">Event ID to update (placeholder)</label>
-      <input id="eventid" name="eventid" type="number" required />
-
       <label htmlFor="newtitle">Title</label>
       <input
         id="newtitle"

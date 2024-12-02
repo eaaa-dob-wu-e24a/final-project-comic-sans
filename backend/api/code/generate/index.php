35d50ed4d87d9e39fb0580fcaf9629f4f321ae74
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2024-12-02 10:55:37 +0100

    remove extra header

--- backend/api/code/generate/index.php
+++ backend/api/code/generate/index.php
@@ -1,9 +1,8 @@
 <?php
 
-// Include necessary headers for CORS
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Credentials: true"); // Allow credentials
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 

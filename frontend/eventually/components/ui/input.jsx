6656fa741926f207cf0edbee7cd091c879b35d0e
siirielli <eaasesli@students.eaaa.dk>
2024-12-02 11:38:30 +0100

    Update input component to always use dark text color

--- frontend/eventually/components/ui/input.jsx
+++ frontend/eventually/components/ui/input.jsx
@@ -4,7 +4,7 @@ export default function Input({ type, placeholder, id, maxLength, className, onC
       type={type}
       placeholder={placeholder}
       maxLength={maxLength}
-      className={`rounded-full text-background py-2 px-4 w-full focus:border-secondary focus:ring-1 focus:ring-secondary focus:bg-white/80 focus:outline-none ${className}`}
+      className={`rounded-full text-dark py-2 px-4 w-full focus:border-secondary focus:ring-1 focus:ring-secondary focus:bg-white/80 focus:outline-none ${className}`}
       name={id}
       id={id}
       onChange={onChange}

1db68b9972ac02937066dfdce9ae41be2789423c
Simone Mikkelsene <simi@youseepost.dk>
2024-12-03 00:08:48 +0100

    Add user session fetching and improve form layout; enhance input handling

--- frontend/eventually/app/createEvent/page.jsx
+++ frontend/eventually/app/createEvent/page.jsx
@@ -22,13 +22,30 @@ const CreateEvent = () => {
 
     const apiUrl = process.env.NEXT_PUBLIC_API_URL + '/api/event/create';
     const joinCodeEndpoint = process.env.NEXT_PUBLIC_API_URL + '/api/code/generate';
+    const userSessionEndpoint = process.env.NEXT_PUBLIC_API_URL + '/api/user/check_session';
 
-    // Function to fetch a new join code
+    const fetchUserSession = async () => {
+        try {
+            const response = await fetch(userSessionEndpoint, {
+                method: 'GET',
+                credentials: 'include',
+            });
+            const data = await response.json();
+
+            if (data.status === "success" && data.user) {
+                setUserName(data.user.name); // Set the username if user is logged in
+            }
+        } catch (error) {
+            console.error('Error fetching user session:', error);
+            // Leave userName as an empty string if fetching fails
+        }
+    };
+
     const fetchJoinCode = async () => {
         try {
             const response = await fetch(joinCodeEndpoint, {
                 method: 'GET',
-                credentials: 'include', // Include cookies if needed
+                credentials: 'include',
             });
 
             if (!response.ok) {
@@ -39,12 +56,13 @@ const CreateEvent = () => {
             setJoinCode(data.code);
         } catch (error) {
             console.error('Error fetching join code:', error);
-            setJoinCode(''); // Fallback in case of error
+            setJoinCode('');
         }
     };
 
     useEffect(() => {
-        fetchJoinCode(); // Fetch join code when the component mounts
+        fetchUserSession(); // Attempt to fetch user data
+        fetchJoinCode(); // Fetch join code
     }, []);
 
     const handleSubmit = async (e) => {
@@ -110,21 +128,21 @@ const CreateEvent = () => {
                 </div>
             </GradientCurve>
 
-            <section className="max-w-6xl w-full p-12 rounded-xl drop-shadow mx-auto flex flex-col text-black place-content-center bg-background my-12">
+            <section className="max-w-6xl w-full p-12 rounded-xl drop-shadow mx-auto flex flex-col color: var(--foreground) place-content-center bg-background my-12">
                 <form onSubmit={handleSubmit}>
                     <div className='flex justify-between mb-8'>
                         <input
                             type="text"
                             id="title"
                             name="title"
-                            className="flex-grow max-w-lg mt-1 text-black hover:border-b-4 hover:border-transparent hover:border-b-gray-300 focus:outline-none font-bold text-2xl"
+                            className="flex-grow max-w-lg mt-1 bg-page-foreground hover:border-b-4 hover:border-transparent hover:border-b-gray-300 focus:outline-none font-bold text-2xl"
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
                             placeholder='Event Name'
                             maxlength="30"
                         />
                         <div className='flex'>
-                            <div id="joinCode" className="hover:border-transparent focus:outline-none mt-1 text-2xl text-black font-bold px-4 py-2  rounded-full">
+                            <div id="joinCode" className="hover:border-transparent focus:outline-none mt-1 text-2xl  font-bold px-4 py-2  rounded-full">
                                 {joinCode || 'Fetching...'}
                             </div>
                             <button type="submit" className="text-white text-m font-bold py-2 px-16 uppercase rounded-full shadow-md hover:cursor-pointer bg-secondary hover:bg-secondary-hover transition-all duration-200r undefined">
@@ -142,7 +160,7 @@ const CreateEvent = () => {
                                     type="text"
                                     id="userName"
                                     name="userName"
-                                    className="max-w-lg w-full rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
+                                    className="max-w-lg w-full rounded-full mt-1 px-4 py-2 border-2 border-grey-300"
                                     value={userName}
                                     onChange={(e) => setUserName(e.target.value)}
                                     placeholder='User Name'
@@ -154,19 +172,19 @@ const CreateEvent = () => {
                                     type="text"
                                     id="location"
                                     name="location"
-                                    className="max-w-lg w-full rounded-full mt-1 px-4 py-2 text-black border-2 border-grey-300"
+                                    className="max-w-lg w-full rounded-full mt-1 px-4 py-2  border-2 border-grey-300"
                                     value={location}
                                     onChange={(e) => setLocation(e.target.value)}
                                     placeholder='Location'
                                 />
                             </div>
                             <div className='flex flex-col min-h-[4rem] w-full'>
-                                <label htmlFor="description" className="block text-lg text-black font-medium">Description:</label>
+                                <label htmlFor="description" className="block text-lg  font-medium">Description:</label>
                                 <textarea
                                     type="text"
                                     id="description"
                                     name="description"
-                                    className="leading-6 rounded-xl w-full mt-1 px-4 py-2 text-black rounded border-2 border-grey-300"
+                                    className="leading-6 rounded-xl w-full mt-1 px-4 py-2 rounded border-2 border-grey-300"
                                     value={description}
                                     onChange={(e) => setDescription(e.target.value)}
                                     placeholder='Description'
@@ -174,30 +192,31 @@ const CreateEvent = () => {
                             </div>
                         </div>
 
-                        <div className='w-3/5 space-y-8'>
+                        <div>
                             {/* Proposed Dates Section */}
-                            <div>
-                                <label className="text-lg font-medium">Proposed Dates:</label>
+
+                            <label className="text-lg font-medium mr-4 color: var(--foreground)">Proposed Dates:</label>
+                            <div className='w-3/5 space-y-4'>
                                 {proposedDates.map((date, index) => (
                                     <div key={index} className="flex space-x-2">
                                         <input
                                             type="datetime-local"
                                             value={date.start}
                                             onChange={(e) => handleProposedDateChange(index, 'start', e.target.value)}
-                                            className="max-w-lg rounded-full mt-2 px-4 py-2 text-black border-2 border-grey-300"
+                                            className="max-w-lg rounded-full  px-4 py-2 text-black border-2 border-grey-300"
                                             placeholder="Start Date"
                                         />
                                         <input
                                             type="datetime-local"
                                             value={date.end}
                                             onChange={(e) => handleProposedDateChange(index, 'end', e.target.value)}
-                                            className="max-w-lg rounded-full mt-2 px-4 py-2 text-black border-2 border-grey-300"
+                                            className="max-w-lg rounded-full  px-4 py-2 text-black border-2 border-grey-300"
                                             placeholder="End Date"
                                         />
                                         <button
                                             type="button"
                                             onClick={() => removeProposedDate(index)}
-                                            className="rounded-full px-2 py-1 bg-red-500 text-background rounded"
+                                            className="rounded-full  px-2 py-1 bg-red-500 text-background rounded"
                                         >
                                             Remove
                                         </button>
@@ -208,7 +227,7 @@ const CreateEvent = () => {
                                     onClick={addProposedDate}
                                     className="rounded-full mt-2 px-4 py-2 bg-blue-500 text-white"
                                 >
-                                    Add Proposed Date
+                                    +
                                 </button>
                             </div>
                         </div>

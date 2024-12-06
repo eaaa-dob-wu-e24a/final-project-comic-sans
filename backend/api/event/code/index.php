1403ae82871c4639b5f93b530074cd4f1b10969c
Simone Mikkelsene <simi@youseepost.dk>
2024-12-06 10:40:50 +0100

    Update CORS policy to restrict access to localhost

--- backend/api/event/code/index.php
+++ backend/api/event/code/index.php
@@ -1,6 +1,6 @@
 <?php
 // Include necessary headers for CORS
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");

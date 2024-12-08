6e6cfb62635f10284137f3abc11deaf774fa460f
siirielli <eaasesli@students.eaaa.dk>
2024-12-08 18:38:18 +0100

    Add size variant for FormLabel component

--- frontend/eventually/components/ui/formlabel.jsx
+++ frontend/eventually/components/ui/formlabel.jsx
@@ -1,8 +1,15 @@
-export default function FormLabel({ htmlFor, children, className }) {
+export default function FormLabel({
+  htmlFor,
+  children,
+  className,
+  variant = "sm",
+}) {
+  const sizeClass = variant === "lg" ? "text-lg" : "text-sm";
+
   return (
     <label
       htmlFor={htmlFor}
-      className={`block text-sm text-foreground py-0.5 ${className}`}
+      className={`block ${sizeClass} text-foreground py-0.5 font-medium ${className}`}
     >
       {children}
     </label>

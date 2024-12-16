86436de06664802c5e0615159b807f8d1d4e0849
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2024-12-16 15:34:03 +0100

    fix corner rounding in texarea

--- frontend/eventually/app/create/page.jsx
+++ frontend/eventually/app/create/page.jsx
@@ -384,7 +384,7 @@ export default function CreateEvent() {
               <textarea
                 id="description"
                 name="description"
-                className="rounded-full shadow-cardshadow text-dark py-2 px-4 w-full focus:border-secondary focus:ring-1 focus:ring-secondary focus:bg-white/80 focus:outline-none"
+                className="rounded-2xl shadow-cardshadow text-dark py-2 px-4 w-full focus:border-secondary focus:ring-1 focus:ring-secondary focus:bg-white/80 focus:outline-none"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
                 placeholder="Description"

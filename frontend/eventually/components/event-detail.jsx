d31092c27b668a8380a007965b5fc746d7d48d9c
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2024-12-16 12:08:07 +0100

    add icons to event page

--- frontend/eventually/components/event-detail.jsx
+++ frontend/eventually/components/event-detail.jsx
@@ -1,5 +1,6 @@
 import Button from "./ui/button";
 import { useNotif } from "./notif-context";
+import Image from "next/image";
 
 export default function EventDetail({ event }) {
   const notif = useNotif();
@@ -21,12 +22,26 @@ export default function EventDetail({ event }) {
       </div>
 
       <div className="flex place-content-between mt-2">
-        <p className="text-primary font-bold text-lg">
-          Location: {event?.Location || "DummyLocation"}
+        <p className="text-primary font-bold text-lg flex flex-nowrap gap-2">
+          <Image
+            src={"/locpin.svg"}
+            height={28}
+            width={28}
+            alt="Map pin icon"
+          ></Image>
+          {event?.Location || "DummyLocation"}
         </p>
         <p>Created by {event?.UserName || "DummyUser"}</p>
       </div>
-      <h2 className="text-primary font-bold text-lg">Description </h2>
+      <h2 className="text-primary font-bold text-lg flex flex-nowrap gap-2 mt-4">
+        <Image
+          src={"/desc.svg"}
+          height={28}
+          width={28}
+          alt="Map pin icon"
+        ></Image>
+        Description
+      </h2>
 
       <div className="shadow-md p-4 rounded-xl">
         <p>{event?.Description}</p>
